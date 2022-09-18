04ff27437747ddad6b2a58c4d0945470a23b3215
miguelSoldado1 <88410268+miguelSoldado1@users.noreply.github.com>
2022-09-18 16:18:37 +0100

    added module export to index.js

--- index.js
+++ index.js
@@ -85,3 +85,5 @@ mongoose
     app.listen(process.env.PORT, () => console.log(`Server running on port: ${process.env.PORT}`));
   })
   .catch((error) => console.log(error.message));
+
+module.exports = app;
