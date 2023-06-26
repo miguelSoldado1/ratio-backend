98826e3675e780b2c552606fcf72eeabded0f14d
miguel <miguelsoldado12@gmail.com>
2023-06-26 20:15:23 +0100

    added rate limiting, might move this to env variables

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,15 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
+import rateLimit from "express-rate-limit";
 import config from "../config";
 import { errorHandlerMiddleware } from "./customError";
 import { authRoutes, albumDetailsRoutes, homeScreenRoutes, navigationBarRoutes, profileScreenRoutes } from "./routes";
 
 const app = express();
 
+// rate limiting for max 100 request per 5 minutes
+app.use(rateLimit({ windowMs: 5 * 60 * 1000, max: 100, standardHeaders: true, legacyHeaders: false }));
 app.use(bodyParser.json({ limit: "30mb" }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
